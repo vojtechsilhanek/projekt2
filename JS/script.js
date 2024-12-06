21996bdccb0d3ccf04a575858db7cceff09f1ec6
Vojtěch Šilhánek <148778457+vojtechsilhanek@users.noreply.github.com>
2024-12-06 12:12:56 +0100

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    tempConverter = function(number) {
+        
+        document.querySelector("#outputCelsius").innerHTML = (number * 1.8) + 32;
+    };
+     
+});
+
+
