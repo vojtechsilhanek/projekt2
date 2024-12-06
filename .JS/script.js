a5985afd2127826d79f6e9f389d025ad49aac7d4
Vojtěch Šilhánek <148778457+vojtechsilhanek@users.noreply.github.com>
2024-12-06 12:07:16 +0100

    Add files via upload

--- .JS/script.js
+++ .JS/script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    window.tempConverter = function(number) {
+    tempConverter = function(number) {
         
         document.querySelector("#outputCelsius").innerHTML = (number * 1.8) + 32;
     };
