400f5d9e0e67433c8a35fce1a4a4ad75016565a0
Vojtěch Šilhánek <148778457+vojtechsilhanek@users.noreply.github.com>
2024-12-06 12:13:10 +0100

    Delete .JS directory

--- .JS/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    tempConverter = function(number) {
-        
-        document.querySelector("#outputCelsius").innerHTML = (number * 1.8) + 32;
-    };
-     
-});
-
-
